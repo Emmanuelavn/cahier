9d651bfc698f8623f09856310dadf6809c0aa8ee
Bryan-lk4 <bryanleke04@gmail.com>
2023-10-02 08:44:13 +0100

    V-0.0 chat gpt attention limiter les essais a cause des tokens

---
+++ DATA/GPT-3/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ezeetest | GPT-3 Chat</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="app">
+    <div id="sidebar">
+      <h3>Contexte</h3>
+      <textarea id="contextInput" rows="10" cols="30">Tu es un super informaticient .Ton but est d'aider des etuiants en informatique a atteindre leurs objectif .tu connait tout les langage de programation et tu peut les enseigners 
+</textarea>
+    </div>
+    <div id="chat">
+      <div id="messages"></div>
+      <input type="text" id="userInput" placeholder="Tapez votre message ici...">
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
---
+++ DATA/GPT-3/script.js
@@ -0,0 +1,69 @@
+//YOUR_OPENAI_API_KEY
+const API_KEY = "YOUR_OPENAI_API_KEY";
+const MODEL = "gpt-3.5-turbo";
+const API_URL = "https://api.openai.com/v1/chat/completions";
+
+const contextInput = document.getElementById("contextInput");
+const userInput = document.getElementById("userInput");
+
+let messageHistory = [
+  {
+    role: "system",
+    content: contextInput.value,
+  },
+];
+
+// Mettre à jour le contexte lorsqu'il est modifié
+contextInput.addEventListener("input", () => {
+  messageHistory[0].content = contextInput.value;
+});
+
+async function sendMessage(userMessage) {
+  messageHistory.push({ role: "user", content: userMessage });
+
+  const data = {
+    model: MODEL,
+    messages: messageHistory,
+  };
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const json = await response.json();
+    return json.choices[0].message.content;
+  } else {
+    throw new Error(`Erreur API: ${response.status}`);
+  }
+}
+
+function appendMessage(content, role) {
+  const messagesDiv = document.getElementById("messages");
+  const message = document.createElement("p");
+  message.innerText = `${role}: ${content}`;
+  messagesDiv.appendChild(message);
+}
+
+userInput.addEventListener("keydown", async (event) => {
+  if (event.key === "Enter") {
+    const userMessage = userInput.value.trim();
+    if (userMessage) {
+      appendMessage(userMessage, "Vous");
+      userInput.value = "";
+
+      try {
+        const assistantResponse = await sendMessage(userMessage);
+        messageHistory.push({ role: "assistant", content: assistantResponse });
+        appendMessage(assistantResponse, "Assistant");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+});
